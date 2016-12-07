4cc4980246b4f144378409f2c9656edafbf33e60
Siddharth Jha <sidchicharito@gmail.com>
2016-12-07 16:26:52 +0530

    Dummy Nodes made

---
+++ high_level_planner/planner.py
@@ -0,0 +1,13 @@
+#planner.py
+#Siddharth S Jha
+
+def planner(data):
+    
+
+def listener();
+    rospy.init_node('planner')
+    rospy.Subscriber('angles', Point32, planner)
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
