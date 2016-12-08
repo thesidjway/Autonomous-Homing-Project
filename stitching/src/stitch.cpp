42f52b5e5d23bb5968dcbdaf52e92dbd1f5cde48
Siddharth Jha <sidchicharito@gmail.com>
2016-12-08 20:33:55 +0530

    Updated README

--- stitching/src/stitch.cpp
+++ stitching/src/stitch.cpp
@@ -21,9 +21,6 @@ void imageCallback(const sensor_msgs::ImageConstPtr &msg)
       return;
     }
 
-    // Draw an example circle on the video stream
-    if (cv_ptr->image.rows > 60 && cv_ptr->image.cols > 60)
-      cv::circle(cv_ptr->image, cv::Point(50, 50), 10, CV_RGB(255,0,0));
 
     // Update GUI Window
     cv::imshow(OPENCV_WINDOW, cv_ptr->image);
