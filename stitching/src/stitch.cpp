a67627cf36eb9ac517b65eea632045b8b2d50d73
Siddharth Jha <sidchicharito@gmail.com>
2016-12-08 20:20:22 +0530

    OpenCV and ARDrone video feed generated

---
+++ stitching/src/stitch.cpp
@@ -0,0 +1,46 @@
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+#include <sensor_msgs/image_encodings.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <cv_bridge/cv_bridge.h>
+#include <image_transport/image_transport.h>
+
+static const std::string OPENCV_WINDOW = "Image window";
+
+void imageCallback(const sensor_msgs::ImageConstPtr &msg) 
+{ 
+    cv_bridge::CvImagePtr cv_ptr;
+    try
+    {
+      cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+    }
+    catch (cv_bridge::Exception& e)
+    {
+      ROS_ERROR("cv_bridge exception: %s", e.what());
+      return;
+    }
+
+    // Draw an example circle on the video stream
+    if (cv_ptr->image.rows > 60 && cv_ptr->image.cols > 60)
+      cv::circle(cv_ptr->image, cv::Point(50, 50), 10, CV_RGB(255,0,0));
+
+    // Update GUI Window
+    cv::imshow(OPENCV_WINDOW, cv_ptr->image);
+    cv::waitKey(3);
+}
+
+int main(int argc, char **argv)
+{
+  
+  ros::init(argc, argv, "stitch_node");
+
+  ros::NodeHandle nh;
+  image_transport::ImageTransport it(nh);
+  image_transport::Subscriber sub = it.subscribe("ardrone/front/image_raw", 10, imageCallback);
+
+
+  ros::spin();
+
+  return 0;
+}
