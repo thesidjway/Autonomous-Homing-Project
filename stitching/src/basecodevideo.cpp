fd3d6b73b688dd242e139c15762d5c2245cd946c
Siddharth Jha <sidchicharito@gmail.com>
2016-12-09 17:25:59 +0530

    Panorama stitching complete

--- stitching/src/basecodevideo.cpp
+++ stitching/src/basecodevideo.cpp
@@ -7,8 +7,10 @@
 # include "opencv2/highgui/highgui.hpp"
 # include "opencv2/nonfree/features2d.hpp"
 
-#define Y_THRESHOLD 2
+#define Y_THRESHOLD 5
 using namespace cv;
+using namespace std;
+
 int distx,disty;
 Mat prevframe,frame;
 Mat img_1,img_2;
@@ -16,7 +18,7 @@ Mat img_1,img_2;
 int main( int argc, char** argv )
 {
   int iterator=0;
-  VideoCapture cap("1.mp4"); 
+  VideoCapture cap("destination.mp4"); 
   std::cout<<cap.get(CV_CAP_PROP_FRAME_COUNT);
   for(iterator=0;iterator<cap.get(CV_CAP_PROP_FRAME_COUNT);iterator++)
   {
@@ -24,7 +26,6 @@ int main( int argc, char** argv )
     img_2=frame;
     if(img_1.empty())
     {
-      std::cout<<"HAAN";
       prevframe=img_2;
     }
     img_1 = prevframe;
@@ -61,8 +62,8 @@ int main( int argc, char** argv )
       if( dist > max_dist ) max_dist = dist;
     }
 
-    printf("-- Max dist : %f \n", max_dist );
-    printf("-- Min dist : %f \n", min_dist );
+    // printf("-- Max dist : %f \n", max_dist );
+    // printf("-- Min dist : %f \n", min_dist );
 
     //-- Draw only "good" matches (i.e. whose distance is less than 2*min_dist,
     //-- or a small arbitary value ( 0.02 ) in the event that min_dist is very
@@ -108,20 +109,34 @@ int main( int argc, char** argv )
     		Rect img2roi(bpt.x,0,img_2.cols-bpt.x,img_1.rows);
     		img2cut=img_2(img2roi);
     		break; 
+
     	}
 
     	//printf( "-- Good Match [%d] Keypoint 1: %d  -- Keypoint 2: %d  \n", i, good_matches[i].queryIdx, good_matches[i].trainIdx ); 
     }
 
+    // imshow("img1", img1cut);
+    // imshow("img2", img2cut);
+
     Size sz1 = img1cut.size();
     Size sz2 = img2cut.size();
+
+  //  cout<<sz1.height<<" "<<sz2.height<<endl;
+
     Mat result(sz1.height, sz1.width+sz2.width, CV_8UC1);
     Mat left(result, Rect(0, 0, sz1.width, sz1.height));
     img1cut.copyTo(left);
-    Mat right(result, Rect(sz1.width, 0, sz2.width, sz2.height));
+    Mat right(result, Rect(sz1.width, 0, sz2.width, sz1.height));
     img2cut.copyTo(right);
-    imshow("result", result);
-    prevframe=img_2;
+    imshow("left", left);
+    imshow("right", right);
+    Mat concat;
+    hconcat(left,right,concat);
+    imshow("result", concat);
+
+    cout<<concat.cols<<endl;
+
+    prevframe=concat;
      if(waitKey(30) == 27) //wait for 'esc' key press for 30 ms. If 'esc' key is pressed, break loop
        {
                 std::cout << "esc key is pressed by user" << std::endl; 
@@ -130,6 +145,5 @@ int main( int argc, char** argv )
 
   }
 
-  return 0;
 }
 
