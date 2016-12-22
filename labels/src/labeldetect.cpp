a1ef203a65bf74901d325d5d3dd2cdffa3f79a7c
Siddharth Jha <sidchicharito@gmail.com>
2016-12-22 23:02:21 +0530

    Rudimentary Label algo

--- labels/src/labeldetect.cpp
+++ labels/src/labeldetect.cpp
@@ -13,33 +13,78 @@
 #include "ardrone_autonomy/Navdata.h"
 #include <mutex>
 #include <algorithm>
+#include <labels/LabelAngles.h>
 
-#define erosion_size 2
-#define DISTTHRES 35
-
-int angles[14]; //RWB,RGB,GWB,BRG,WRB,BGR,WBR,WRG,GBR,BWR,WGR,WGB,WBG,GWR
+float angles[14]; //RWB,RGB,GWB,BRG,WRB,BGR,WBR,WRG,GBR,BWR,WGR,WGB,WBG,GWR
 using namespace cv;
 using namespace std;
+float currAngle;
+mutex currAngleLock, angleArrayLock;
 
-
+labels::LabelAngles angleMessage;
 
 float dist(Point2f a1,Point2f a2)
 {
-  return sqrt(pow(a1.x-a2.x,2)+pow(a2.y-a1.y,2));
+  return sqrt(pow(a2.x-a1.x,2)+pow(a2.y-a1.y,2));
 }
 
+int isnonzero(float* array, int arraysize)
+{
+    for(int i=0;i<arraysize;i++)
+    {
+        if (fabs(array[i])<0.001)
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+void detectPoster(std::vector<KeyPoint> KR, std::vector<KeyPoint> KB, std::vector<KeyPoint> KG)
+{
+    if(KR.size()>0)
+    {
+        if(KB.size()>0)
+        {
+            if(KG.size()>0) //RGB
+            {
+                
+                cout<<"BGR"<<endl;
+
+            }
+            else //RB
+            {
+                cout<<"BR"<<endl;
+
+            }
+        }
+        else if(KG.size()>0) //RG
+        {
+            cout<<"GR"<<endl;
+
+        }
+        //cout<<"JUST RED."<<endl;
+    }
+    else if(KB.size()>0) //BG
+    {
+        if(KG.size()>0)
+        {
+            cout<<"BG"<<endl;
+        }
+        //cout<<"JUST BLUE."<<endl;
+    }
+    else
+    {
+        //cout<<"JUST GREEN OR NOTHING."<<endl;
+    }
+}
 void imageCallback(const sensor_msgs::ImageConstPtr &msg) 
 { 
 
     Rect myROI(260,0,120,360);
     Mat srcred1,srcred2,srcred,srcdark,srcblue,srcgreen,srcwhite;
-
-    //Mat boundary[6];//boundaryGB, boundaryRW, boundaryGW, boundaryBW, boundaryRG, boundaryBR;
     Mat detectionImgFull,detectionImg;
-
-    Mat detectionImgHSV,detectionImgGray;//,detectionImgHLS;
-
-    Mat erodedblue,erodedred;
+    Mat detectionImgHSV,detectionImgGray;
 
     cv_bridge::CvImagePtr cv_ptr;
 
@@ -53,12 +98,9 @@ void imageCallback(const sensor_msgs::ImageConstPtr &msg)
       return;
     }
 
-    Mat element = getStructuringElement( MORPH_CROSS,Size( 2*erosion_size + 1, 2*erosion_size+1 ), Point(erosion_size, erosion_size ) );
-
     detectionImgFull=cv_ptr->image;
     detectionImg=detectionImgFull(myROI);
 
-    //cout<<detectionImg.rows<<" "<<detectionImg.cols<<endl;
     cvtColor(detectionImg,detectionImgHSV,CV_BGR2HSV);
     cvtColor(detectionImg,detectionImgGray,CV_BGR2GRAY);
 
@@ -71,44 +113,25 @@ void imageCallback(const sensor_msgs::ImageConstPtr &msg)
 
     inRange(detectionImgGray,0,99,srcdark);
     inRange(detectionImgGray,100,255,srcwhite);
-    // dilate( srcdark, dilatedWhite, element );
-    // dilate( srcred, dilatedRed, element );
-    // dilate( srcblue, dilatedBlue, element );
-    // dilate( srcgreen, dilatedGreen, element );
+
     bitwise_and(srcdark,srcblue,srcblue);
     bitwise_and(srcdark,srcgreen,srcgreen);
-    //bitwise_and(srcdark,srcred,srcred);
-
-    erode( srcblue, erodedblue, element );
-    erode( srcred, erodedred, element );
-
 
 
 
     SimpleBlobDetector::Params params;
 
     params.filterByColor = true;
-
     params.blobColor = 255;
-
     params.minThreshold = 200;
-
     params.filterByArea = true;
-    params.minArea = 10;
-    params.minArea = 200;
-     
-    // Filter by Circularity
+    params.minArea = 50;
     params.filterByCircularity = false;
-    
-    //params.minCircularity = 0.1;
-    // Filter by Convexity
     params.filterByConvexity = true;
     params.minConvexity = 0.30;    
-    // Filter by Inertia
     params.filterByInertia = true;
     params.maxInertiaRatio = 0.8;
 
-    // Set up detector with params
     SimpleBlobDetector detector(params);
 
     std::vector<KeyPoint> keypointsred,keypointsblue,keypointsgreen,keypointswhite;
@@ -117,53 +140,39 @@ void imageCallback(const sensor_msgs::ImageConstPtr &msg)
     detector.detect( srcred, keypointsred);
     detector.detect( srcblue, keypointsblue);
     detector.detect( srcgreen, keypointsgreen);
-  //  detector.detect( srcdark, keypointswhite);
 
-// DrawMatchesFlags::DRAW_RICH_KEYPOINTS flag ensures the size of the circle corresponds to the size of blob
     Mat im_with_keypoints_red,im_with_keypoints_white,im_with_keypoints_green,im_with_keypoints_blue;
+
+    Mat blankImg(360,120,CV_8UC3,Scalar(255,255,255));
     
-    drawKeypoints( detectionImg, keypointsred, im_with_keypoints_red, Scalar(0,0,255), DrawMatchesFlags::DRAW_RICH_KEYPOINTS );
-    drawKeypoints( detectionImg, keypointsblue, im_with_keypoints_blue, Scalar(0,0,255), DrawMatchesFlags::DRAW_RICH_KEYPOINTS );
-    drawKeypoints( detectionImg, keypointsgreen, im_with_keypoints_green, Scalar(0,0,255), DrawMatchesFlags::DRAW_RICH_KEYPOINTS );
-    // drawKeypoints( detectionImg, keypointswhite, im_with_keypoints_white, Scalar(0,0,255), DrawMatchesFlags::DRAW_RICH_KEYPOINTS );
+    // drawKeypoints( detectionImg, keypointsred, im_with_keypoints_red, Scalar(0,0,255), DrawMatchesFlags::DRAW_RICH_KEYPOINTS );
+    // drawKeypoints( detectionImg, keypointsblue, im_with_keypoints_blue, Scalar(0,0,255), DrawMatchesFlags::DRAW_RICH_KEYPOINTS );
+    // drawKeypoints( detectionImg, keypointsgreen, im_with_keypoints_green, Scalar(0,0,255), DrawMatchesFlags::DRAW_RICH_KEYPOINTS );
 
-    // drawKeypoints( srcred, keypointsred, im_with_keypoints_red, Scalar(0,0,255), DrawMatchesFlags::DRAW_RICH_KEYPOINTS );
-    // drawKeypoints( srcblue, keypointsblue, im_with_keypoints_blue, Scalar(0,0,255), DrawMatchesFlags::DRAW_RICH_KEYPOINTS );
-    // drawKeypoints( srcgreen, keypointsgreen, im_with_keypoints_green, Scalar(0,0,255), DrawMatchesFlags::DRAW_RICH_KEYPOINTS );
-    //drawKeypoints( detectionImg, keypointswhite, im_with_keypoints_white, Scalar(0,0,255), DrawMatchesFlags::DRAW_RICH_KEYPOINTS );
- 
-  //  imshow("ORIGINAL",detectionImg);
-// Show blobs
-    imshow("keypointsR", im_with_keypoints_red);
-    imshow("keypointsG", im_with_keypoints_green);
-    imshow("keypointsB", im_with_keypoints_blue);
-  //  imshow("keypointsW", im_with_keypoints_white);
+    drawKeypoints( blankImg, keypointsred, blankImg, Scalar(0,0,255), DrawMatchesFlags::DRAW_RICH_KEYPOINTS );
+    drawKeypoints( blankImg, keypointsblue, blankImg, Scalar(255,0,0), DrawMatchesFlags::DRAW_RICH_KEYPOINTS );
+    drawKeypoints( blankImg, keypointsgreen, blankImg, Scalar(0,255,0), DrawMatchesFlags::DRAW_RICH_KEYPOINTS );
 
-    int k=0,m=0;
+    // imshow("keypointsR", im_with_keypoints_red);
+    // imshow("keypointsG", im_with_keypoints_green);
+    // imshow("keypointsB", im_with_keypoints_blue);
+    imshow("Colour",blankImg);
+    imshow("Original",detectionImg);
 
-    for(k=0;k<keypointsblue.size();k++)
-    {
-      keypointsBG.push_back(keypointsblue[k].pt);
-      keypointsRB.push_back(keypointsblue[k].pt);
-      keypointsall.push_back(keypointsblue[k].pt);
-    }
 
-    for(k=0;k<keypointsred.size();k++)
-    {
-      keypointsRG.push_back(keypointsred[k].pt);
-      keypointsRB.push_back(keypointsred[k].pt);
-      keypointsall.push_back(keypointsred[k].pt);
-    }
-    for(k=0;k<keypointsgreen.size();k++)
-    {
-      keypointsRG.push_back(keypointsgreen[k].pt);
-      keypointsBG.push_back(keypointsgreen[k].pt);
-      keypointsall.push_back(keypointsgreen[k].pt);
-    }
+    detectPoster(keypointsred,keypointsgreen,keypointsblue);
 
-    cout<<"#############"<<endl;
+    cv::waitKey(20);
 
-    cv::waitKey(30);
+    
+
+}
+
+void NavcallBack(const ardrone_autonomy::Navdata::ConstPtr& msg) 
+{
+  currAngleLock.lock();
+  currAngle=msg->rotZ;
+  currAngleLock.unlock();
 }
 
 int main(int argc, char **argv)
@@ -174,9 +183,33 @@ int main(int argc, char **argv)
   ros::NodeHandle nh;
   image_transport::ImageTransport it(nh);
   image_transport::Subscriber sub = it.subscribe("ardrone/front/image_raw", 10, imageCallback);
+  ros::Subscriber navSub = nh.subscribe <ardrone_autonomy::Navdata>("/ardrone/navdata", 100, NavcallBack); 
+  ros::Publisher anglePub = nh.advertise <labels::LabelAngles>("labelAngles",100);
+  ros::Rate loop_rate(50);
 
-
-  ros::spin();
-
+  while(ros::ok())
+  {
+    angleArrayLock.lock();
+    if(isnonzero(angles,14)==1)
+    {
+        angleMessage.RWB = angles[0];
+        angleMessage.RGB = angles[1];
+        angleMessage.GWB = angles[2];
+        angleMessage.BRG = angles[3];
+        angleMessage.WRB = angles[4]; 
+        angleMessage.BGR = angles[5];
+        angleMessage.WBR = angles[6];
+        angleMessage.WRG = angles[7]; 
+        angleMessage.GBR = angles[8]; 
+        angleMessage.BWR = angles[9]; 
+        angleMessage.WGR = angles[10]; 
+        angleMessage.WGB = angles[11]; 
+        angleMessage.WBG = angles[12]; 
+        angleMessage.GWR = angles[13];
+        anglePub.publish(angleMessage);
+    }
+    angleArrayLock.unlock();
+    ros::spin();
+  }
   return 0;
 }
